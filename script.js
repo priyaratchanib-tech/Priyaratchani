137d17933502e096fba6ea78b1d6faaa40ff5716
priyaratchanib-tech <priyaratchanib@gmail.com>
2025-09-08 10:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+js
+
+Replace pannunga with:
+
+// Typewriter Effect
+
+const text = "B.Sc CS Student | Aspiring Software Developer | Future Zoho Engineer";
+
+let i = 0;
+
+function typeWriter() {
+
+  if (i < text.length) {
+
+    document.getElementById("tagline").innerHTML += text.charAt(i);
+
+    i++;
+
+    setTimeout(typeWriter, 80);
+
+  }
+
+}
+
+typeWriter();
+
+// Button click alert
+
+function sayHello() {
+
+  alert("Hello! Thanks for visiting my portfolio 😊");
+
+}
